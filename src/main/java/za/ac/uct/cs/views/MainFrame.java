16a0340ce3071a59f897e9fd967407dbe52ee9c6
SteveWang7596 <37341748+SteveWang7596@users.noreply.github.com>
2021-07-28 17:16:48 +0200

    Allow import owl file, added OWL api 5 dependency to build file
    Allow import owl file, added OWL api 5 dependency to build file

--- src/main/java/za/ac/uct/cs/views/MainFrame.java
+++ src/main/java/za/ac/uct/cs/views/MainFrame.java
@@ -14,11 +14,16 @@ import javax.swing.JFileChooser;
  */
 public class MainFrame extends javax.swing.JFrame {
 
+    private File owl_file;
+    private String owl_file_path;
+    
     /**
      * Creates new form MainFrame
      */
     public MainFrame() {
         initComponents();
+        owl_file = null;
+        owl_file_path = "";
     }
 
     /**
@@ -31,12 +36,45 @@ public class MainFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         FileChooser = new javax.swing.JFileChooser();
+        jPanel1 = new javax.swing.JPanel();
+        lblOwlFile = new javax.swing.JLabel();
+        txtOwlFilePath = new javax.swing.JTextField();
         MenuBar = new javax.swing.JMenuBar();
         FileMenu = new javax.swing.JMenu();
         ImportOWLFile = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        lblOwlFile.setText("OWL File: ");
+
+        txtOwlFilePath.setEditable(false);
+        txtOwlFilePath.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtOwlFilePathActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblOwlFile, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtOwlFilePath, javax.swing.GroupLayout.DEFAULT_SIZE, 560, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblOwlFile)
+                    .addComponent(txtOwlFilePath, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(354, Short.MAX_VALUE))
+        );
 
         FileMenu.setText("File");
 
@@ -59,11 +97,15 @@ public class MainFrame extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 279, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -73,7 +115,23 @@ public class MainFrame extends javax.swing.JFrame {
         JFileChooser fileChooser = FileChooser;
         fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
         int result = fileChooser.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION)
+        {
+            owl_file = fileChooser.getSelectedFile();
+            owl_file_path = owl_file.getPath();
+            txtOwlFilePath.setText(owl_file_path);
+            System.out.println("Ontology File: " + owl_file.getName() + ".");
+            System.out.println("From path: " + owl_file_path);
+        }
+        else
+        {
+            System.out.println("User cancelled owl file selection process.");
+        }
     }//GEN-LAST:event_ImportOWLFileActionPerformed
+
+    private void txtOwlFilePathActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtOwlFilePathActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtOwlFilePathActionPerformed
 
     /**
      * @param args the command line arguments
@@ -116,5 +174,8 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JMenuItem ImportOWLFile;
     private javax.swing.JMenuBar MenuBar;
     private javax.swing.JMenu jMenu2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblOwlFile;
+    private javax.swing.JTextField txtOwlFilePath;
     // End of variables declaration//GEN-END:variables
 }
